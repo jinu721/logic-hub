2dae6d97088e496c9d9161edd72d8e8b2bbdb639
jinu <jinu8683@gmail.com>
2025-07-16 17:02:33 +0530

    feat(constants): Backend Constants Added

---
+++ backend/src/constants/messages.ts
@@ -0,0 +1,7 @@
+export const AUTH_MESSAGES = {
+    USER_EXISTS: "User with this email or username already exists",
+    USER_NOT_FOUND: "User not found",
+    INVALID_CREDENTIALS: "Invalid credentials",
+    INVALID_OTP: "Invalid OTP",
+    VERIFICATION_SENT: "Email verification sent",
+};
